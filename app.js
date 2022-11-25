605fffd9ce59dce798917e13c8781005bdbcfb02
Alexis Morantin <alexismorantin44390@gmail.com>
2022-11-25 15:09:21 +0100

    JS number card random

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let cardNumber = document.querySelector(".container_card_number");
+let cardDate = document.querySelector(".container_card_date");
 let cardName = document.querySelector(".container_card_name");
 
 fetch("https://randomuser.me/api/")
@@ -6,9 +8,24 @@ fetch("https://randomuser.me/api/")
         let title = data.results[0].name.title;
         let firstName = data.results[0].name.first;
         let lastName = data.results[0].name.last;
-        // console.log(title);
-        // console.log(firstName);
-        // console.log(lastName);
 
         cardName.textContent = title + " " + firstName + " " + lastName;
+        function numberRamdom(min, max) {
+            return (
+                Math.ceil(Math.random() * (max - min) + min) +
+                " " +
+                Math.ceil(Math.random() * (max - min) + min) +
+                " " +
+                Math.ceil(Math.random() * (max - min) + min) +
+                " " +
+                Math.ceil(Math.random() * (max - min) + min) +
+                " " +
+                Math.ceil(Math.random() * (max - min) + min)
+            );
+        }
+        function dateRandom() {
+
+        }
+        cardNumber.textContent = numberRamdom(1000, 9999);
+
     });
